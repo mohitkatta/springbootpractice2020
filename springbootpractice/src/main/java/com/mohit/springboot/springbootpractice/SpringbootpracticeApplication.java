0149cf8c8752d5d2ba5d4ce1005f178b6f7ce55c
mohitkatta <43848781+mohitkatta@users.noreply.github.com>
2020-05-19 08:20:15 +0530

    Add files via upload

---
+++ springbootpractice/src/main/java/com/mohit/springboot/springbootpractice/SpringbootpracticeApplication.java
@@ -0,0 +1,14 @@
+package com.mohit.springboot.springbootpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootpracticeApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
